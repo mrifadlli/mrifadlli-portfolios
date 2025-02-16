ec7e3aa4d74d7171e4189fc245037c065a4e87bf
mrifadlli <manricofadli@gmail.com>
2025-02-16 21:13:10 +0700

    update

--- components/magicui/dock.tsx
+++ components/magicui/dock.tsx
@@ -33,13 +33,13 @@ const Dock = React.forwardRef<HTMLDivElement, DockProps>(
     const mousex = useMotionValue(Infinity);
 
     const renderChildren = () => {
-      return React.Children.map(children, (child: any) => {
-        if (React.isValidElement(child)) {
+      return React.Children.map(children, (child) => {
+        if (React.isValidElement<DockIconProps>(child)) {
           return React.cloneElement(child, {
             mousex,
             magnification,
             distance,
-          } as DockIconProps);
+          });
         }
         return child;
       });
@@ -62,17 +62,15 @@ const Dock = React.forwardRef<HTMLDivElement, DockProps>(
 Dock.displayName = "Dock";
 
 export interface DockIconProps {
-  size?: number;
   magnification?: number;
   distance?: number;
-  mousex?: any;
+  mousex?: ReturnType<typeof useMotionValue>;
   className?: string;
   children?: React.ReactNode;
   props?: PropsWithChildren;
 }
 
 const DockIcon = ({
-  size,
   magnification = DEFAULT_MAGNIFICATION,
   distance = DEFAULT_DISTANCE,
   mousex,
@@ -87,13 +85,13 @@ const DockIcon = ({
     return val - bounds.x - bounds.width / 2;
   });
 
-  let widthSync = useTransform(
+  const widthSync = useTransform(
     distanceCalc,
     [-distance, 0, distance],
     [40, magnification, 40]
   );
 
-  let width = useSpring(widthSync, {
+  const width = useSpring(widthSync, {
     mass: 0.1,
     stiffness: 150,
     damping: 12,
